105493ab4f1dbdaf47bc486a18d4b17a5ec364df
jinyang <jinyang>
2019-12-01 22:33:40 +0800

    bootstrap改善页面， 2.在页面上实现etcd键值对的查询和增加功能

--- src/web/controllers/etcd_manager.go
+++ src/web/controllers/etcd_manager.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
+	"../../services"
 	"fmt"
 	"github.com/kataras/iris"
-	"../../services"
 	"github.com/kataras/iris/mvc"
+	"strings"
 )
 
 type EtcdManangerController struct {
@@ -14,14 +15,43 @@ type EtcdManangerController struct {
 }
 
 var v = mvc.View{
-	Name:"conf_manager.html",
+	Name: "conf_manager.html",
 }
 
-func (e *EtcdManangerController) Get() mvc.Result{
+func (e *EtcdManangerController) Get() mvc.Result {
 	return v
 }
 
-func  (e *EtcdManangerController)GetAdd(){
+func (e *EtcdManangerController) Post() mvc.Response {
+	k := e.Ctx.FormValue("key")
+	v := e.Ctx.FormValue("value")
+	fmt.Println("key:", k, ", value:", v)
+
+	e.Service.PutKV(k, v)
+	return mvc.Response{
+		Text: "ok",
+	}
+}
+
+func (e *EtcdManangerController) GetKv() string {
+	k := e.Ctx.FormValue("k")
+
+	resp := e.Service.Get(k)
+
+	var v strings.Builder
+
+	for _, ev := range resp.Kvs {
+		// return result is not string
+		v.WriteString(string(ev.Value))
+		fmt.Printf("etcd key = %s , etcd value = %s", ev.Key, ev.Value)
+	}
+
+	fmt.Println("k:",k, "v:", v)
+
+	return  "v:"+v.String()
+}
+
+func (e *EtcdManangerController) GetAdd() {
 	etcdKey := "/logagent/192.168.0.142/logconfig"
 
 	etcdValue := `
